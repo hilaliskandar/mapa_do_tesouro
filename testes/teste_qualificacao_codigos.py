3b9e3b3a334a2e0ec50eb2cf45a6ed89a099bf6c
Carlos Alexandre Gomes <106932072+hilaliskandar@users.noreply.github.com>
2026-08-04 05:23:07 -0300

    Testa subtotais estruturais e limpeza de rotulos

--- testes/teste_qualificacao_codigos.py
+++ testes/teste_qualificacao_codigos.py
@@ -8,19 +8,21 @@ from processamentos.qualificar_codigos import qualificar_codigos
 def _base_receitas() -> pd.DataFrame:
     return pd.DataFrame(
         {
-            "bloco": ["receitas"] * 5,
-            "aba_origem": ["Receitas"] * 5,
-            "codigo_ibge": ["3500000"] * 5,
-            "municipio": ["Municipio A"] * 5,
-            "ano": [2024] * 5,
-            "estagio": ["Realizadas"] * 5,
-            "codigo_conta": ["1.1.1.2.50", pd.NA, pd.NA, pd.NA, pd.NA],
+            "bloco": ["receitas"] * 7,
+            "aba_origem": ["Receitas"] * 7,
+            "codigo_ibge": ["3500000"] * 7,
+            "municipio": ["Municipio A"] * 7,
+            "ano": [2024] * 7,
+            "estagio": ["Realizadas"] * 7,
+            "codigo_conta": ["1.1.1.2.50", pd.NA, pd.NA, pd.NA, pd.NA, pd.NA, pd.NA],
             "descricao_conta": [
                 "IPTU",
                 "Total das Receitas",
                 "Descricao sem codigo",
                 "1.7.2.8.01.1.0 Cota-Parte do ICMS",
                 "7.2.1.5.01.0.0 Contribuicao patronal intraorcamentaria",
+                "RECEITAS (EXCETO INTRA-ORCAMENTARIAS) (I)",
+                "1.7.1.8.03.0.0\nTransferencia de Recursos do SUS ¿",
             ],
             "rotulo_conta_original": [
                 "Realizadas | 1.1.1.2.50 - IPTU",
@@ -28,10 +30,12 @@ def _base_receitas() -> pd.DataFrame:
                 "Descricao sem codigo",
                 "Realizadas | 1.7.2.8.01.1.0 Cota-Parte do ICMS",
                 "Realizadas | 7.2.1.5.01.0.0 Contribuicao patronal intraorcamentaria",
+                "Receitas brutas | RECEITAS (EXCETO INTRA-ORCAMENTARIAS) (I)",
+                "Realizadas | 1.7.1.8.03.0.0\nTransferencia de Recursos do SUS ¿.1",
             ],
-            "origem_metadados": ["dicionario"] * 5,
-            "valor": [100.0, 300.0, 50.0, 200.0, 80.0],
-            "valor_original": [100.0, 300.0, 50.0, 200.0, 80.0],
+            "origem_metadados": ["dicionario"] * 7,
+            "valor": [100.0, 300.0, 50.0, 200.0, 80.0, 500.0, 60.0],
+            "valor_original": [100.0, 300.0, 50.0, 200.0, 80.0, 500.0, 60.0],
         }
     )
 
@@ -47,13 +51,15 @@ def teste_qualificacao_separa_pendencias_e_funcao(tmp_path: Path) -> None:
     despesas = receitas.assign(
         bloco="despesas",
         aba_origem="Despesas",
-        codigo_conta=["3.3.90.30.00", pd.NA, pd.NA, "3.3.91.39.00", pd.NA],
+        codigo_conta=["3.3.90.30.00", pd.NA, pd.NA, "3.3.91.39.00", pd.NA, pd.NA, pd.NA],
         descricao_conta=[
             "Material de Consumo",
             "Total das Despesas",
             "Descricao sem codigo",
             "Outros Servicos de Terceiros",
             "Descricao sem codigo 2",
+            "DESPESAS (EXCETO INTRAORCAMENTARIAS)",
+            "Descricao sem codigo 3",
         ],
         rotulo_conta_original=[
             "Empenhadas | 3.3.90.30.00 - Material de Consumo",
@@ -61,6 +67,8 @@ def teste_qualificacao_separa_pendencias_e_funcao(tmp_path: Path) -> None:
             "Descricao sem codigo",
             "Empenhadas | 3.3.91.39.00 - Outros Servicos de Terceiros",
             "Descricao sem codigo 2",
+            "Empenhadas | DESPESAS (EXCETO INTRAORCAMENTARIAS)",
+            "Descricao sem codigo 3",
         ],
     )
     despesas.to_parquet(origem / "despesas_longo.parquet", index=False)
@@ -68,13 +76,15 @@ def teste_qualificacao_separa_pendencias_e_funcao(tmp_path: Path) -> None:
     funcao = receitas.assign(
         bloco="despesa_por_funcao",
         aba_origem="Despesa por funcao",
-        codigo_conta=["15.451", pd.NA, pd.NA, pd.NA, pd.NA],
+        codigo_conta=["15.451", pd.NA, pd.NA, pd.NA, pd.NA, pd.NA, pd.NA],
         descricao_conta=[
             "Infraestrutura Urbana",
             "Total da Funcao",
             "Descricao sem codigo",
             "FU12 - Demais Subfuncoes",
             "26.782 Transporte Rodoviario",
+            "DESPESAS (INTRA-ORCAMENTARIAS)",
+            "Descricao sem codigo 2",
         ],
         rotulo_conta_original=[
             "Empenhadas | 15.451 - Infraestrutura Urbana",
@@ -82,6 +92,8 @@ def teste_qualificacao_separa_pendencias_e_funcao(tmp_path: Path) -> None:
             "Descricao sem codigo",
             "Empenhadas | FU12 - Demais Subfuncoes",
             "Empenhadas | 26.782 Transporte Rodoviario",
+            "Pagas | DESPESAS (INTRA-ORCAMENTARIAS)",
+            "Descricao sem codigo 2",
         ],
     )
     funcao.to_parquet(origem / "despesa_por_funcao_longo.parquet", index=False)
@@ -95,9 +107,15 @@ def teste_qualificacao_separa_pendencias_e_funcao(tmp_path: Path) -> None:
     assert quadro_receitas.loc[3, "codigo_conta"] == "1.7.2.8.01.1.0"
     assert quadro_receitas.loc[3, "origem_codigo_qualificado"] == "extraido_do_texto"
     assert quadro_receitas.loc[4, "natureza_operacao"] == "intraorcamentaria"
+    assert quadro_receitas.loc[5, "tipo_registro"] == "subtotal_estrutural"
+    assert not bool(quadro_receitas.loc[5, "pendencia_revisao"])
+    assert quadro_receitas.loc[5, "natureza_operacao"] == "orcamentaria"
+    assert quadro_receitas.loc[6, "codigo_conta"] == "1.7.1.8.03.0.0"
 
     quadro_despesas = pd.read_parquet(saida / "despesas_qualificado.parquet")
     assert quadro_despesas.loc[3, "natureza_operacao"] == "intraorcamentaria"
+    assert quadro_despesas.loc[5, "tipo_registro"] == "subtotal_estrutural"
+    assert quadro_despesas.loc[5, "natureza_operacao"] == "orcamentaria"
 
     quadro_funcao = pd.read_parquet(saida / "despesa_por_funcao_qualificado.parquet")
     assert quadro_funcao.loc[0, "codigo_funcao"] == "15"
@@ -111,3 +129,6 @@ def teste_qualificacao_separa_pendencias_e_funcao(tmp_path: Path) -> None:
     assert quadro_funcao.loc[4, "codigo_conta"] == "26.782"
     assert quadro_funcao.loc[4, "codigo_funcao"] == "26"
     assert quadro_funcao.loc[4, "codigo_subfuncao"] == "782"
+    assert quadro_funcao.loc[5, "tipo_registro"] == "subtotal_estrutural"
+    assert quadro_funcao.loc[5, "natureza_operacao"] == "intraorcamentaria"
+    assert not bool(quadro_funcao.loc[5, "pendencia_revisao"])
